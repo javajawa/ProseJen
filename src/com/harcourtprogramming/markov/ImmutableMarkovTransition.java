540ed3780c2744e582c2509e329ea1b882aa0583
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-05-14 20:00:16 +0100

    Fixes for ImmutableMarkovTransition contructor to meet tests

--- src/com/harcourtprogramming/markov/ImmutableMarkovTransition.java
+++ src/com/harcourtprogramming/markov/ImmutableMarkovTransition.java
@@ -16,6 +16,8 @@ public final class ImmutableMarkovTransition<S> implements IMarkovTransition<S>
 	 */
 	ImmutableMarkovTransition(float weighting, S destinationState)
 	{
+		if (destinationState == null) throw new IllegalArgumentException("Destition State must be non-null");
+		if (weighting <= 0) throw new IllegalArgumentException("Weight must be a postive value");
 		this.weighting = weighting;
 		this.destinationState = destinationState;
 	}
